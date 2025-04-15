30bc9dd2bd316f9b50835a5c490a367b26cb7f90
Kyrylo Petrenko <kyrylo.petrenko1@nure.ua>
2025-04-15 12:38:02 +0200

    Update code-examples.java

--- Pract2/pzpi-22-8-petrenko-kyrylo-software-architecture/code-examples.java
+++ Pract2/pzpi-22-8-petrenko-kyrylo-software-architecture/code-examples.java
@@ -1,1 +1,23 @@
+class Message {
+   private String sender;
+   private String receiver;
+   private String content;
+  
+   public Message(String sender, String receiver, String content) {
+     this.sender = sender;
+     this.receiver = receiver;
+     this.content = content;
+    }
+  
+    public String toString() {
+      return sender + " → " + receiver + ": " + content;
+    }
+}
+  
+class MessageService {
+     public void sendMessage(Message message) {
+        System.out.println("Sending: " + message);
+        // Тут могла б бути логіка збереження в базу або надсилання через WebSocket
+     }
+}
 
