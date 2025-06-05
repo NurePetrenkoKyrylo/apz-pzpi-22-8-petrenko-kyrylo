e544f2efbb4284bad3e797eb09bb44a8a22d364b
Kyrylo Petrenko <kyrylo.petrenko1@nure.ua>
2025-06-05 11:35:00 +0200

    Add files via upload

---
+++ Lab2/pzpi-22-8-petrenko-kyrylo-lab2/config.js
@@ -0,0 +1,5 @@
+//isoooiso226 GFOZxEwLFaCLMpgj
+module.exports = {
+    secret:"secretWebToken",
+    connection:"mongodb+srv://isoooiso226:GFOZxEwLFaCLMpgj@atarc.lnadk.mongodb.net/?retryWrites=true&w=majority&appName=ATARC"
+}
